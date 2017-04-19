45c86577583af4fe7bcd7e727f2592e9eddc3bfb
qjr <qinjr@icloud.com>
2017-04-19 21:39:28 +0800

    build visual graph

--- PktRelGraph/pktRelGraphD3js.py
+++ PktRelGraph/pktRelGraphD3js.py
@@ -3,7 +3,7 @@ import json
 
 cap = pyshark.FileCapture("httpPkts.pcapng", display_filter = "http", only_summaries = True)
 G = {'nodes':[], 'links':[]}
-maxTimeGap = 0.001
+maxTimeGap = 0.1
 pktsList = []
 
 def buildList():
@@ -23,14 +23,20 @@ def giveWeight(pkt1, pkt2):
     pkt2Dst = pkt2.destination
     if pkt1Dst == pkt2Dst:
         weight += 1
+    
+    #feature3:source ip
+    pkt1Src = pkt1.source
+    pkt2Src = pkt2.source
+    if pkt1Src == pkt2Src:
+        weight += 1
     return weight
 
-def buildGraph():
+def buildVisualGraph():
     global G
     pktsListLength = len(pktsList)
     #add nodes
     for i in range(pktsListLength):
-        G['nodes'].append({'id':pktsList[i].time})
+        G['nodes'].append({'id':pktsList[i].time, 'group':i})
     for i in range(pktsListLength):
         pkt1 = pktsList[i]
         for j in range(i + 1, pktsListLength):
@@ -39,8 +45,9 @@ def buildGraph():
             if weight12 > 0:
                 G['links'].append({'source':pkt1.time, 'target':pkt2.time, 'value':weight12})
 
+
 buildList()
-buildGraph()
+buildVisualGraph()
 f = open("rel.json", "w")
 f.write(json.dumps(G))
 f.close()
