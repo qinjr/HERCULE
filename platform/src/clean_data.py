f864dcca3bdc7558eb4c99fb77640707391ce507
qjr <qinjr@icloud.com>
2017-05-18 20:55:02 +0800

    clean data

--- platform/src/clean_data.py
+++ platform/src/clean_data.py
@@ -2,10 +2,6 @@ def clean(old_logfile, new_logfile, target_labels):
     old = open(old_logfile, 'r')
     new = open(new_logfile, 'w')
 
-    normal_count = 0
-    ddos_count = 0
-    others_count = 0
-
     while True:
         log_string = old.readline()
         if not log_string:
@@ -14,19 +10,13 @@ def clean(old_logfile, new_logfile, target_labels):
         label = features[-1]
         if label[:-2] in target_labels:
             if label[:-2] == "normal":
-                normal_count += 1
                 new.writelines([log_string])
             else:
-                ddos_count += 1
                 features[-1] = "ddos.\n"
                 log_string = ','.join(features)
                 new.writelines([log_string])
         else:
-            others_count += 1
             continue
-    print("normal log count: ", normal_count)
-    print("ddos log count: ", ddos_count)
-    print("other log count: ", others_count)
     old.close()
     new.close()
 
@@ -34,7 +24,15 @@ def clean(old_logfile, new_logfile, target_labels):
 def main():
     target_labels = ["normal", "apache2", "back", "land", "mailbomb", "neptune", "pod", "processtable", "smurf",
                     "teardrop", "udpstorm"]
-    clean("../data/kddcup.data_10_percent_corrected.log", "../data/labeled_ddos_logs.log", target_labels)
+    clean("../data/raw_test_data/corrected.log", "../data/test_data/corrected.log", target_labels)
+    print("complete raw_test_data/corrected.log")
+    
+    clean("../data/raw_training_data/kddcup.data_10_percent_corrected.log", "../data/training_data/kddcup.data_10_percent_corrected.log", target_labels)
+    print("complete raw_training_data/kddcup.data_10_percent_corrected.log")
+    clean("../data/raw_training_data/kddcup.data.corrected.log", "../data/training_data/kddcup.data.corrected.log", target_labels)
+    print("complete raw_training_data/kddcup.data.corrected.log")
+    
+    print("data cleaning finished")
 
 if __name__ == '__main__':
     main()
